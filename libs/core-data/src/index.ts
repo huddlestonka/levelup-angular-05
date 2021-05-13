e6a7414ea0381547d248c817e25c6dfb386d3d56
Kaleb  Huddleston <nanny@kalebs-air.lan>
2021-05-13 10:29:22 -0700

    Foodie and meals services configured

--- libs/core-data/src/index.ts
+++ libs/core-data/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/core-data.module';
+export { FoodiesService } from './lib/foodies/foodies.service';
+export { MealsService } from './lib/meals/meals.service';
--- libs/core-data/src/lib/meals/meals.service.ts
+++ libs/core-data/src/lib/meals/meals.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Meal } from '@bba/api-interfaces';
+import { environment } from '@env/environment';
+
+const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MealsService {
+  model = 'meals';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  all() {
+    return this.http.get<Meal[]>(this.getUrl());
+  }
+
+  find(id: string) {
+    return this.http.get<Meal>(this.getUrlWithId(id));
+  }
+
+  create(meal: Meal) {
+    return this.http.post(this.getUrl(), meal, { headers: headers });
+  }
+
+  update(meal: Meal) {
+    return this.http.put(this.getUrlWithId(meal.id), meal, {
+      headers: headers,
+    });
+  }
+
+  delete(meal: Meal) {
+    return this.http.delete(this.getUrlWithId(meal.id));
+  }
+
+  private getUrl() {
+    return `${environment.apiEndpoint}${this.model}`;
+  }
+
+  private getUrlWithId(id) {
+    return `${this.getUrl()}/${id}`;
+  }
 }
