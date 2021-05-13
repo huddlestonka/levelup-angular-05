2a85f71787afb0796c5da85143baec0bdb394473
Kaleb  Huddleston <nanny@kalebs-air.lan>
2021-05-13 10:14:44 -0700

    Entities decided

---
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -0,0 +1,25 @@
+export interface BaseEntity {
+  id: string | null;
+  title?: string | null;
+}
+
+export interface Foodie extends BaseEntity {
+  name: string;
+  nickName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  type: string;
+  meals?: Meal[];
+}
+
+export interface Meal extends BaseEntity {
+  title: string;
+  description: string;
+  type: string;
+  time: string;
+  appetizer?: string;
+  mainDish?: string;
+  dessert?: string;
+  foodieId: string;
+}
